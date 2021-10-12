60d1e6eef7173404014b102b9a46c7f037a5aa51
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-10-12 20:02:14 +0300

    #2 Install multi-website Magento instance: added env files and DB dump

---
+++ app/etc/env.php
@@ -0,0 +1,86 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '5cfe9e2319d7b0c1252769162a277da0'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'nadiia_zlobynets_magento_local',
+                'username' => 'nadiia_zlobynets_magento_local',
+                'password' => 'nadiia_zlobynets_magento_local',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'nadiia-zlobynets-magento.local'
+    ],
+    'install' => [
+        'date' => 'Sun, 10 Oct 2021 20:21:54 +0000'
+    ]
+];
