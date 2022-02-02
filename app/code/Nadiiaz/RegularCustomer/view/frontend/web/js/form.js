5c25d4853e60cab132b0a4bcc82ed475daf6d28a
Nadiia.Zlobynets <euphoria93@gmail.com>
2022-02-02 22:48:58 +0200

    Lesson 12 - jQuery widgets: edited form.js and templates fot form and form open button

--- app/code/Nadiiaz/RegularCustomer/view/frontend/web/js/form.js
+++ app/code/Nadiiaz/RegularCustomer/view/frontend/web/js/form.js
@@ -10,7 +10,7 @@ define([
     $.widget('Nadiiaz.personalDiscount_form', {
         options: {
             action: '',
-            isModal: true
+            isModal: false
         },
 
         /**
@@ -23,30 +23,43 @@ define([
                 $(this.element).modal({
                     buttons: []
                 });
-
                 $(document).on('nadiiaz_personal_discount_form_open', this.openModal.bind(this));
             }
         },
 
+        /**
+         * Open modal dialog
+         */
         openModal: function () {
             $(this.element).modal('openModal');
         },
 
+        /**
+         * Validate form and send request
+         */
         sendRequest: function () {
-            if (!this.validate()) {
+            if (!this.validateForm()) {
                 return;
             }
+
             this.ajaxSubmit();
         },
 
-        validate: function () {
+        /**
+         * Validate request form
+         */
+        validateForm: function () {
             return $(this.element).validation().valid();
         },
 
+        /**
+         * Submit request via AJAX. Add form key to the post data.
+         */
         ajaxSubmit: function () {
             let formData = new FormData($(this.element).get(0));
 
-            // formData.append('form_key');
+            // Form key is not appended when the form is in the tab. Must add it manually
+            formData.append('form_key', $.mage.cookies.get('form_key'));
             formData.append('isAjax', 1);
 
             $.ajax({
@@ -58,10 +71,16 @@ define([
                 dataType: 'json',
                 context: this,
 
+                /** @inheritdoc */
                 beforeSend: function () {
                     $('body').trigger('processStart');
                 },
 
+                /**
+                 * Success means that response from the server was received, but not that the request was saved!
+                 *
+                 * @inheritdoc
+                 */
                 success: function (response) {
                     alert({
                         title: $.mage.__('Posting your request...'),
@@ -69,19 +88,23 @@ define([
                     });
                 },
 
+                /** @inheritdoc */
                 error: function () {
                     alert({
                         title: $.mage.__('Error'),
-                        content: $.mage.__('Your request can\'t be sent. Please contact us if you see this message')
+                        content: $.mage.__('Your request can\'t be sent. Please, contact us if you see this message.')
                     });
                 },
 
+                /** @inheritdoc */
                 complete: function () {
-                    $(this.element).modal('closeModal');
+                    if (this.options.isModal) {
+                        $(this.element).modal('closeModal');
+                    }
+
                     $('body').trigger('processStop');
                 }
-            })
-
+            });
         }
     });
 
