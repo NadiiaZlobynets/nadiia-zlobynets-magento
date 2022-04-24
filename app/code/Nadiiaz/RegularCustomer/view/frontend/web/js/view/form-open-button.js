f45dda30dff431ac26860d288c1d48792c73a2e0
Nadiia.Zlobynets <euphoria93@gmail.com>
2022-04-24 22:07:32 +0100

    Lesson 15 - Knockout.js UI Components: Extracted form submit into a separate action script

--- app/code/Nadiiaz/RegularCustomer/view/frontend/web/js/view/form-open-button.js
+++ app/code/Nadiiaz/RegularCustomer/view/frontend/web/js/view/form-open-button.js
@@ -1,18 +1,22 @@
 define([
     'jquery',
-    'jquery/ui'
-], function ($) {
+    'uiComponent',
+    'Nadiiaz_RegularCustomer_formSubmitRestrictions'
+], function ($, Component, formSubmitRestrictions) {
     'use strict';
 
-    $.widget('Nadiiaz.personalDiscount_formOpenButton', {
-        _create: function () {
-            $(this.element).on('click.nadiiaz_personal_discount_form_open', this.openRequestForm.bind(this));
+    return Component.extend({
+        defaults: {
+            template: 'Nadiiaz_RegularCustomer/form-open-button'
         },
 
+        formSubmitIsRestricted: formSubmitRestrictions.formSubmitDeniedMessage,
+
+        /**
+         * Generate event to open the form
+         */
         openRequestForm: function () {
             $(document).trigger('nadiiaz_personal_discount_form_open');
         }
     });
-
-    return $.Nadiiaz.personalDiscount_formOpenButton;
 });
