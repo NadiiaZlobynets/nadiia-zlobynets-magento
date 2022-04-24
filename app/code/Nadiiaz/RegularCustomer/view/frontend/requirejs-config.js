91c8b3fdced883bc56f1827732c760bbc128e21c
Nadiia.Zlobynets <euphoria93@gmail.com>
2022-04-24 21:40:56 +0100

    Lesson 15 - Knockout.js UI Components: Extracted form submit into a separate action script

--- app/code/Nadiiaz/RegularCustomer/view/frontend/requirejs-config.js
+++ app/code/Nadiiaz/RegularCustomer/view/frontend/requirejs-config.js
@@ -1,8 +1,9 @@
 var config = {
     map: {
         '*': {
-            'Nadiiaz_RegularCustomer_form': 'Nadiiaz_RegularCustomer/js/form',
-            'Nadiiaz_RegularCustomer_formOpenButton': 'Nadiiaz_RegularCustomer/js/form-open-button'
+            'Nadiiaz_RegularCustomer_form': 'Nadiiaz_RegularCustomer/js/view/form',
+            'Nadiiaz_RegularCustomer_formOpenButton': 'Nadiiaz_RegularCustomer/js/view/form-open-button',
+            'Nadiiaz_RegularCustomer_submitFormAction': 'Nadiiaz_RegularCustomer/js/action/submit-form',
         }
     }
 };
