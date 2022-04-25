ba98bf5d9aec429ce921d89f05a0f46d236812c3
Nadiia.Zlobynets <euphoria93@gmail.com>
2022-04-25 12:15:40 +0100

    Lesson 15: added a cutom menu to admin

---
+++ app/code/Nadiiaz/RegularCustomer/Controller/Adminhtml/Create/Index.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Nadiiaz\RegularCustomer\Controller\Adminhtml\Create;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Nadiiaz_RegularCustomer::create';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/custommenu_create_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Create menu'));
+
+        return $resultPage;
+    }
+}
